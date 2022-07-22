3b150f3dc8fd7ecce42884d7d1d0fd9f479d3ba2
dabuzida <dabuzida@gmail.com>
2022-07-22 18:02:32 +0900

    1

--- lib/main.dart
+++ lib/main.dart
@@ -20,16 +20,51 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.teal,
           centerTitle: true,
         ),
-        body: Center(
-          child: Column(children: <Widget>[
-            Icon(Icons.block, color: Colors.red),
-            SizedBox(height: 50),
-            Container(
-              decoration: BoxDecoration(border: Border.all(color: Colors.green, width: 1)),
-              child: SizedBox.shrink(),
-            ),
-          ]),
-        ),
+        body: DropdownBtn(),
+      ),
+    );
+  }
+}
+
+class DropdownBtn extends StatefulWidget {
+  const DropdownBtn({Key? key}) : super(key: key);
+
+  @override
+  State<DropdownBtn> createState() => _DropdownBtnState();
+}
+
+class _DropdownBtnState extends State<DropdownBtn> {
+  String dropdownValue = '전체보기';
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: 500,
+            height: 300,
+            color: Colors.blue,
+          ),
+          DropdownButton<String>(
+            value: dropdownValue,
+            // icon: const Icon(Icons.arrow_downward),
+            elevation: 12,
+            style: const TextStyle(color: Colors.deepPurple),
+            underline: SizedBox.shrink(),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue = newValue!;
+              });
+            },
+            items: <String>['전체보기', '알츠윈', '알츠윈 컨설턴트', '보건소 전화', '보건소 대면'].map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
